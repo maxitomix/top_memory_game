99452e3259df06cced25302a0a811a3fe2d9201a
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-30 10:30:21 -0400

    photos from API working. Now lets build a game!

--- src/App.js
+++ src/App.js
@@ -2,53 +2,95 @@ import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
 import csvFile from './data/AirlineData.csv';
 
-const NUM_PLANES = 1
+const NUM_PLANES = 3
 
-
-
-const fetchPlane = (registration) => {
-   
-  const planeRegistration = registration;
-
-  const url = `https://api.planespotters.net/pub/photos/reg/${planeRegistration}`;
-
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();   // This returns a promise
-    })
-    .then(data => {
-      if (data.photos && data.photos.length === 0) {
-        console.log('Empty object returned');
-      return false;
-      }
-      console.log(data);
-      console.log(data.photos[0].thumbnail_large.src);
-      return data.photos[0].thumbnail_large.src
-    })
-    .catch(error => console.log('There was an error!', error));
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
+function getRandomElements(arr, n) {
+  let result = new Set();
+  let registrations = new Set();
+  let callsignPrefixes = new Set();
+
+  while(result.size < n && arr.length > result.size) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const candidate = arr[randomIndex];
+
+    const registration = candidate.Registration;
+    const callsignPrefix = candidate.Callsign.slice(0, 2);
+
+    if (!registrations.has(registration) && !callsignPrefixes.has(callsignPrefix)) {
+      registrations.add(registration);
+      callsignPrefixes.add(callsignPrefix);
+      result.add(candidate); 
+    }
+  }
+
+  return [...result];
+}
+
+
+
+async function fetchPlane(registration) {
+  const url = `https://api.planespotters.net/pub/photos/reg/${registration}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.photos && data.photos.length > 0) {
+      return data.photos[0].thumbnail_large.src;
+    }
+
+    return null;
+  } catch (error) {
+    console.log('There was an error!', error);
+    return null;
+  }
+}
 
 
 
 function App() {
   const [data, setData] = useState([]);
-  const [RandomPlanes, setRandomPlanes] = useState([]);
+  const [randomPlanes, setRandomPlanes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     Papa.parse(csvFile, {
       download: true,
       header: true,
-      complete: (parsedData) => {
+      complete: async function(parsedData) {
         setData(parsedData.data);
-        const randomPlanes = getRandomElements(parsedData.data, NUM_PLANES);
-        setRandomPlanes(randomPlanes);
+        await delay(1000);
+        const validPlanes = [];
+        while (validPlanes.length < NUM_PLANES) {
+          await delay(1000);
+          const [candidate] = getRandomElements(parsedData.data, 1);
+          const imageUrl = await fetchPlane(candidate.Registration);
+
+            if (imageUrl) {
+              validPlanes.push({...candidate, url: imageUrl});
+            }
+
+            await delay(1000);
+
+            if (validPlanes.length >= NUM_PLANES) {
+              break;
+            }
+          }
+        
+        setRandomPlanes(validPlanes);
+
         setIsLoading(false);
+
       }
     });
   }, []);
@@ -58,43 +100,19 @@ function App() {
   }
 
 
-  function getRandomElements(arr, n) {
-    let result = new Set();
-    let registrations = new Set();
-    let callsignPrefixes = new Set();
-  
-    while(result.size < n && arr.length > result.size) {
-      const randomIndex = Math.floor(Math.random() * arr.length);
-      const candidate = arr[randomIndex];
-  
-      const registration = candidate.Registration;
-      const callsignPrefix = candidate.Callsign.slice(0, 2);
-  
-      if (!registrations.has(registration) && !callsignPrefixes.has(callsignPrefix)) {
-        registrations.add(registration);
-        callsignPrefixes.add(callsignPrefix);
-
-        // if (fetchPlane(registration)){
-        //   candidate.push(url:fetchPlane(registration))
-          result.add(candidate);
-        // }
-      }
-    }
-  
-    return [...result];
-  }
 
 
   return (
     <div className="App">
       <header className="App-header">
 
-        {RandomPlanes.map((plane, index) => (
+        {randomPlanes.map((plane, index) => (
           <>
           <p key={index}>
             {JSON.stringify(plane, null, 2)}
           </p>
-          <button onClick={fetchPlane}>Fetch Planes</button>
+          <img src={plane.url} alt="" />
+          
           </>
         ))}
 
